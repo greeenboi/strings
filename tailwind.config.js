86b691e53859d9f3915f28854d1453a7cd62cb7b
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-12-17 15:57:13 +0530

    Update colors and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,8 +31,8 @@ module.exports = {
         'dark-4': '#1F1F22',
         'light-1': '#FFFFFF',
         'light-2': '#EFEFEF',
-        'light-3': '#7878A3',
-        'light-4': '#5C5C7B',
+        'light-3': '#009387',
+        'light-4': '#007A86',
       },
       screens: {
         'xs': '480px',
